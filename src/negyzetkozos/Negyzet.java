7e35cfeb5d6acaefb3cfedc58a89f0d849ffd51e
Hidy97 <csaladi.alexandra@gmail.com>
2023-10-09 08:33:26 +0200

    Négyzet alapok

--- src/negyzetkozos/Negyzet.java
+++ src/negyzetkozos/Negyzet.java
@@ -1,5 +1,23 @@
 package negyzetkozos;
 
 public class Negyzet {
+    static int oldal = 6;
+    static int koordX = 5;
+    static int koordY = 3;
+    
+    public static void allapot(){
+        System.out.println("A négyzet oldalának hossza:" + oldal);
+    }
+    
+    public static void terulet(){
+        double ter = Math.pow(oldal, oldal);
+        
+        System.out.println("A négyzet terület: " + ter);
+    }
+    
+    @Override
+    public String toString() {
+        return "A négyzet állapota{" + "oldal=" + oldal + ", koordX=" + koordX + ", koordY=" + koordY + '}';
+    }
     
 }
--- src/negyzetkozos/NegyzetKozos.java
+++ src/negyzetkozos/NegyzetKozos.java
@@ -5,6 +5,14 @@ public class NegyzetKozos {
     public static void main(String[] args) {
         //Családi Alexandra
         //Dreilinger Maja
+                
+        feladat();
+    }
+
+    private static void feladat() {
+        Negyzet negyz = new Negyzet();
+        negyz.terulet();
+        negyz.allapot();
     }
     
 }
