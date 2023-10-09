6ec439f1a5588af385a8ee4de91bc3f47de1c7ec
DVMaja <majadreilinger@gmail.com>
2023-10-09 08:31:28 +0200

    kocka osztály kész, kiíródik

--- src/negyzetkozos/Negyzet.java
+++ src/negyzetkozos/Negyzet.java
@@ -1,5 +1,20 @@
 package negyzetkozos;
 
 public class Negyzet {
+    static int a;
+    static double x, y;
+    
+    double melyikKoordinata(){
+        return 3.0;
+    }
+    
+    double terulet(){    
+        return Math.pow(a, 2);
+    }    
+
+    @Override
+    public String toString() {
+        return "Negyzet{" + "Terület =" + terulet() + "oldal hossza =" + a +", x=" + x + ", y=" + y + '}';
+    }
     
 }
--- src/negyzetkozos/NegyzetKozos.java
+++ src/negyzetkozos/NegyzetKozos.java
@@ -1,10 +1,17 @@
 package negyzetkozos;
 
 public class NegyzetKozos {
-    
+
     public static void main(String[] args) {
-        //Családi Alexandra
-        //Dreilinger Maja
+        NegyzetKozos.feladat();
     }
-    
+
+    private static void feladat() {
+        Negyzet negyzet = new Negyzet();
+        negyzet.a = 3;
+        negyzet.x = 5;
+        negyzet.y = 8;
+        System.out.println(negyzet.toString());
+
+    }
 }
